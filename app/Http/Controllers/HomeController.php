46f2b65bcbbca18e135f329cffa97da7056d818b
mayank07feb <makedy07feb@gmail.com>
2024-12-30 17:25:27 +0530

    3 pages:

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,16 @@ class HomeController extends Controller
 
     public function combos()
     {
-        return view('frontend.comobs'); 
+        return view('frontend.comobs');
     }
+
+    public function bestSellers()
+    {
+        return view('frontend.collections.bestsellers');
+    }
+
+    public function newLaunches()
+{
+    return view('frontend.collections.new-launches');
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/combos', [HomeController::class, 'combos'])->name('combos');
+
+
+Route::prefix('collections')->group(function () {
+    Route::get('/best-sellers', [HomeController::class, 'bestSellers'])->name('collections.best-sellers');
+    Route::get('/new-launches', [HomeController::class, 'newLaunches'])->name('collections.new-launches');
+    Route::get('/ultimate-combo', [HomeController::class, 'ultimateCombo'])->name('collections.ultimate-combo');
+    Route::get('/combos', [HomeController::class, 'combos'])->name('collections.combos');
+    Route::get('/office-combos', [HomeController::class, 'officeCombos'])->name('collections.office-combos');
+    Route::get('/ergonomic-chairs', [HomeController::class, 'ergonomicChairs'])->name('collections.ergonomic-chairs');
+});
